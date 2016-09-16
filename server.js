68cd9a2e62342f1a7502ef0dddd4c0f8cb879dcf
Dlaosb <Dlaosb@gmail.com>
2016-09-16 14:29:02 -0700

    Finished backend

--- server.js
+++ server.js
@@ -3,12 +3,24 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, './')));
-app.use(express.static(path.join(__dirname, '/client/')));
+// app.use(express.static(path.join(__dirname, './')));
+// app.use(express.static(path.join(__dirname, '/client/')));
+
+
+// path.join (needs __dirname) versus path.resolve (doesn't need dirname)
+// res.sendFile(pathname)
+// res.sendFile(__dirname)
+
 
 app.use((req, res) => {
   res.sendStatus(404);
 });
+
+app.get('/', (req, res) => {
+	//res.sendFile(path.resolve('./client/index.html'));
+	res.sendFile(path.join(__dirname + '/client/index.html'));
+})
+	
 
 
 app.listen(3000);
